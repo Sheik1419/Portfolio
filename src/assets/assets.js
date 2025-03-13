22f160b35f304026c5978fd96941b1ad13dffa82
Sheik1419 <sheikgamer567@gmail.com>
2025-03-13 17:37:46 +0530

    file upload

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,7 +5,7 @@ import send from "./send.svg";
 import work2 from "./profile1.jpg";
 import laptop from "./laptop.png";
 import web from "./web-design.png";
-import responsive from "./smartphone-call.png"
-import resume from "./Sheik_Shah_Shuaib_Resume.pdf"
+import responsive from "./smartphone-call.png";
+import resume from "./Sheik_Shah_Shuaib Resume.pdf";
 
 export { hand, files, send, scroll, work2, laptop, web, responsive, resume };
