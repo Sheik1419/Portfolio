ccf28a5e7a812964c0a13dd4371f6f7530d88fed
Sheik1419 <sheikgamer567@gmail.com>
2025-03-13 17:13:28 +0530

    file upload

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,17 +1,35 @@
-import aditya from "./Aditya.png"
-import dlr from "./DLR.png"
-import expert from "./Expert.png"
-import ayush from "./Ayush.png"
-import lm from "./LM.png"
-import nulinz from "./Nulinz.png"
-import excoso from "./Excoso.png"
-import neptune from "./Neptune.png"
-import helping from "./Helping.png"
-import fobas from "./Fobas.png"
-import nuscma from "./Nuscma.png"
-import rootments from "./Rootments.png"
-import superhomes from "./Superhomes.png"
-import technomerates from "./Technomerates.png"
-import onstru from "./Onstru.png"
+import aditya from "./Aditya.png";
+import dlr from "./DLR.png";
+import expert from "./Expert.png";
+import ayush from "./Ayush.png";
+import lm from "./LM.png";
+import nulinz from "./Nulinz.png";
+import excoso from "./Excoso.png";
+import neptune from "./Neptune.png";
+import helping from "./Helping.png";
+import fobas from "./Fobas.png";
+import nuscma from "./Nuscma.png";
+import rootments from "./Rootments.png";
+import superhomes from "./Superhomes.png";
+import technomerates from "./Technomerates.png";
+import onstru from "./Onstru.png";
+import sitesync from "./Sitesync.png";
 
-export { aditya, dlr, expert, ayush, lm, nulinz, excoso, neptune, fobas, helping, nuscma, rootments, superhomes, technomerates, onstru }
+export {
+  aditya,
+  dlr,
+  expert,
+  ayush,
+  lm,
+  nulinz,
+  excoso,
+  neptune,
+  fobas,
+  helping,
+  nuscma,
+  rootments,
+  superhomes,
+  technomerates,
+  onstru,
+  sitesync,
+};
