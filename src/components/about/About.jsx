bcbec612d8c27a66d37f58883cf0c3a011ce4c8a
Sheik1419 <sheikgamer567@gmail.com>
2025-03-13 17:41:26 +0530

    file upload

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
           <p className="about__description">
             Frontend Web Developer with 8 months of experience crafting responsive, dynamic, and user-friendly web interfaces using HTML, CSS, JavaScript, ReactJS, jQuery, and Bootstrap. Adept at building cross-browser compatible apps and collaborating to deliver high-quality solutions.
           </p>
-          <a download="Sheik_Shah_Shuaib Resume.pdf" href={resume} className="button button--flex">
+          <a download="Sheik_Shah_Shuaib_Resume.pdf" href={resume} className="button button--flex">
             Download CV
             <img src={files} alt="" className="button__icon" />
           </a>
