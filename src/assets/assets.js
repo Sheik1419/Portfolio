1409f8ef01719096b47b66a62fe3175e9fb54dbb
SheikShuaib <sheikshah@dreaminfinity.in>
2026-09-25 19:15:23 +0530

    Update portfolio

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,7 +2,7 @@ import hand from "./hand.svg";
 import files from "./files.svg";
 import scroll from "./scroll.svg";
 import send from "./send.svg";
-import work2 from "./profile1.jpg";
+import work2 from "./profile_3.jpg";
 import laptop from "./laptop.png";
 import web from "./web-design.png";
 import responsive from "./smartphone-call.png";
